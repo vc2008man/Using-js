862627a2c6a522a04766c78e8f167b0525996929
孙嘉伟 <sunjiawei1986@163.com>
2012-08-25 10:51:22 +0800

    UsingJS.0.5.0
    update some functions and change default module file from js to
    usingmodules

---
+++ 0.5.0/out/api.js
@@ -0,0 +1,30 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Using",
+        "Using.Class"
+    ],
+    "modules": [
+        "Using.Class",
+        "Using.Config",
+        "Using.Modules"
+    ],
+    "allModules": [
+        {
+            "displayName": "Using.Class",
+            "name": "Using.Class",
+            "description": "UsingJS中封装的用于创建类的对象"
+        },
+        {
+            "displayName": "Using.Config",
+            "name": "Using.Config",
+            "description": "用于配置模块对应JS文件的路径 省去.js后缀"
+        },
+        {
+            "displayName": "Using.Modules",
+            "name": "Using.Modules",
+            "description": "可用于模块的命名空间"
+        }
+    ]
+} };
+});

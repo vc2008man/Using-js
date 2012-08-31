c255df491db9865499e9fc6873cd4cd25ed1cc5f
孙嘉伟 <sunjiawei1986@163.com>
2012-08-31 20:26:45 +0800

    Revert "UsingJS.0.5.0"
    This reverts commit 862627a2c6a522a04766c78e8f167b0525996929.

--- 0.5.0/usingmodules/using.config.js
+++
@@ -1,9 +0,0 @@
-/**
-* 用于配置模块对应JS文件的路径 省去.js后缀
-* @module Using.Config
-*/
-Using.Config = {
-	"UsingClass" : "/usingmodules/using.class.0.1",
-	"jquery" : "/js/jquery.min",
-	"login" : "/usingmodules/user.login"
-};
